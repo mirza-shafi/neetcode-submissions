0693b82cf3f1d0c528b1d2e83b3ace6f2199622a
MIRZA MD. SHAFI UDDIN <138498831+mirza-shafi@users.noreply.github.com>
2026-06-08 03:02:30 +0600

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            # Using (left + right) // 2 works fine in Python since it handles arbitrarily large integers,
+            # but left + (right - left) // 2 prevents overflow in other statically-typed languages.
+            mid = left + (right - left) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return -1
